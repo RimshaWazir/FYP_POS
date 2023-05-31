05615479e6ca1368c85df2da2f1877706c66d3e7
Rimsha Wazir <rimshawazir48@gmail.com>
2023-05-30 23:23:43 -0700

    first commit

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,15 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/app/colors.dart';
-import 'package:flutter_application_1/screens/customer/add_customer.dart';
-import 'package:flutter_application_1/screens/customer/manage_customer.dart';
 import 'package:flutter_application_1/screens/product/items.dart';
-import 'package:flutter_application_1/screens/product/manage_product.dart';
 
 import 'responsive_layout/constant.dart';
-import 'screens/product/add_product_widget.dart';
 import 'widgets/dashboard_body.dart';
-import 'widgets/drawerheader.dart';
-import 'widgets/expansion_tile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
